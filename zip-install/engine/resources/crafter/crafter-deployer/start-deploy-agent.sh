1ad51c0986959724e66fb2518df6729c62bc557d
Carlos Ortiz <carlos.ortiz@craftercms.org>
2017-10-20 17:18:18 -0600

    Upgrade Tomcat and deployer config. Added default security Policies.

--- zip-install/engine/resources/crafter/crafter-deployer/start-deploy-agent.sh
+++ zip-install/engine/resources/crafter/crafter-deployer/start-deploy-agent.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-nohup java -server  -Djava.ext.dirs=. -classpath .:conf org.craftercms.cstudio.publishing.PublishingReceiverMain >> deployment.out 2>&1&
+
+if [ "$(whoami)" == "root" ]; then
+	echo -e "\033[38;5;196m"
+    echo "Do not run CrafterCMS as root"
+    echo -e "\033[0m"
+	exit 1
+fi
+
+nohup java -server -Djava.ext.dirs=. -classpath .:conf org.craftercms.cstudio.publishing.PublishingReceiverMain >> deployment.out 2>&1&
--- zip-install/studio/resources/crafter/crafter-deployer/start-deploy-agent.sh
+++ zip-install/studio/resources/crafter/crafter-deployer/start-deploy-agent.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-nohup java -server  -Djava.ext.dirs=. -classpath .:conf org.craftercms.cstudio.publishing.PublishingReceiverMain >> deployment.out 2>&1&
+
+if [ "$(whoami)" == "root" ]; then
+	echo -e "\033[38;5;196m"
+	echo -e "Crafter CMS cowardly refuses to run as root."
+    echo -e "Running Crafter CMS as root is extremely dangerous."
+    echo -e "\033[0m"
+	exit 1
+fi
+
+export deployerSecureHome=$(dirname "${BASH_SOURCE[0]}")
+nohup java -server "-Ddeployer.home=$deployerSecureHome" -Djava.security.manager -Djava.security.policy==crafter.policy -Djava.ext.dirs=. -classpath .:conf org.craftercms.cstudio.publishing.PublishingReceiverMain >> deployment.out 2>&1&
